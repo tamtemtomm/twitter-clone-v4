aa8dbeeb24f7c7c32d37e69fec54f791950a1553
Tamtemtom020604 <timo.widyanvolta-2021@ftmm.unair.ac.id>
2024-08-06 16:05:39 +0700

    Add Deploy preparation

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
-import path from "path"
+import path from "path";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -16,8 +16,8 @@ cloudinary.config({
 // Import the routes
 import authRoutes from "./routes/auth.route";
 import userRoutes from "./routes/user.route";
-import postRoutes from "./routes/post.route"
-import notificationRoutes from "./routes/notification.route"
+import postRoutes from "./routes/post.route";
+import notificationRoutes from "./routes/notification.route";
 
 import connectMongoDB from "./db/ConnectMongoDB";
 
@@ -26,20 +26,30 @@ const PORT = process.env.PORT || 5000;
 // const __dirname = path.resolve()
 
 // Connect to essential middlewares
-app.use(express.json({limit:"5mb"}));
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true })); // to parse from ulencoded data
-app.use(cors({}));
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    cors({
+      origin: ["https://twitter-clone-api-sage.vercel.app"],
+      methods: ["POST", "GET", "DELETE"],
+      credentials: true,
+    })
+  );
+} else {
+  app.use(cors({}));
+}
+
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
-app.use("/api/posts", postRoutes)
-app.use("/api/notifications", notificationRoutes)
+app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
-// if(process.env.NODE_ENV === "production"){
-//   app.use(express.static(path.join(__dirname, "..","..","client", "dist")))
-//   app.get("*", (req, res) => {res.sendFile(path.resolve(__dirname,"..","..","client", "dist", "index.html"))})
-// }
+app.get("/", (req, res) => {
+  res.json("Hello");
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
