adab44435686923607769de3b6cf59cddb93c365
Tamtemtom020604 <timo.widyanvolta-2021@ftmm.unair.ac.id>
2024-08-07 23:07:37 +0700

    Remove static file

--- server/index.ts
+++ server/index.ts
@@ -39,14 +39,14 @@ app.get("/*", (req, res) => {
   res.json({ message: "hello" });
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "..", "..", "client", "dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "..", "..", "client", "dist", "index.html")
-    );
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "..", "..", "client", "dist")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(
+//       path.resolve(__dirname, "..", "..", "client", "dist", "index.html")
+//     );
+//   });
+// }
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
